7aa31702ce892201beac9e1e808623454f2241e3
Sarah Nelson <saradippity26@aol.com>
2019-08-05 11:06:23 -0600

    Add if, elsif

--- control_flow.py
+++ control_flow.py
@@ -7,3 +7,25 @@ if True:
     print("It is true")
 
 print("Done")
+
+if bool("eggs"):
+#checks to see if the string is empty. its not so it is true
+    print("Yes please!")
+
+if "eggs":
+    print("Yes, why not")
+
+#Flat is better than nested (Python: Simple is easier to read, get away from incrypted syntax
+h = 42
+if h > 50:
+    print("Greater than 50")
+    if h > 100:
+        print("Greater than 100")
+elif h < 50: # Else If
+    print("Less than 50")
+else:
+    print("Equals 50")
+
+
+
+print("Done")
