b0408362a2a16ee40068017903cb78bf14481a62
Sarah Nelson <saradippity26@aol.com>
2019-08-05 10:54:55 -0600

    Add if statement

---
+++ control_flow.py
@@ -0,0 +1,9 @@
+#dot string documentation """ documentation of the file itself:
+""" Learn about control flow in Python"""
+
+#IF statement parenthesis are not required in python: True capital T means keyword
+if True:
+    #indentation is 4 spaces
+    print("It is true")
+
+print("Done")
