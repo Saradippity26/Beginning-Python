2b490c8e520f2e6961533b497f7c45b6fffc8209
Sarah Nelson <saradippity26@aol.com>
2019-08-05 12:50:52 -0600

    strings

--- strings.py
+++ strings.py
@@ -20,4 +20,18 @@ s4 = "This is \na multi-line\nstring"  #\n means new line therefore no spaces af
 print(s4)
 #support for backslash (if you want to print the backslash)
 s5 = "A\\in a string"
-print(s5) # py is unicode not assci all characters are two bytes 
+print(s5) # py is unicode not assci all characters are two bytes
+s6 = 'this "wow'
+print(s6)
+#raw strings for python: tells to take string as is (exactly as you meant it to be displayed).  has to prefix with an r
+raw_string = r'C:\User\Documents\Books'
+print(raw_string)
+#print would hide all \\ raw will display them
+#string sequence: and array of characters
+s = "parrot"
+print("s[4]", s[4], type(s))
+#index notation is for representing the 'O' # begins at zero index notation 0,1,2,etc. [displays specific character]
+#lets say we want to capitalize the first letter only. Search for a method
+help(str)
+print(s,s.capitalize())
+
