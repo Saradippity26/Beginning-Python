5f8ad1d6178694d7094a7ea91acfcb9e71dd9038
saradippity26 <saradippity26@aol.com>
2019-08-08 15:59:52 -0600

    formatting fix

--- use_airtravel.py
+++ use_airtravel.py
@@ -32,10 +32,10 @@ def console_card_printer(passenger, seat, flight_number, aircraft):
     Isolate the code to print the boarding pass
     :return:
     """
-    output = "| Name: {0}" \
-             "Flight: {1}" \
-             "Seat: {2}" \
-             "Aircraft: {3}" \
+    output = "| Name: {0} " \
+             "Flight: {1} " \
+             "Seat: {2} " \
+             "Aircraft: {3} " \
              " |".format(passenger, flight_number, seat, aircraft)
     banner = "+" + "-" * (len(output) -2) + "+"
     border = " |" + " " * (len(output) -2) + " |"
