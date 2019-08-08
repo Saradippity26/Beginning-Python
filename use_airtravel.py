24a2b70b1f82ee199e1a3531c261b94ed104c502
saradippity26 <saradippity26@aol.com>
2019-08-08 16:01:58 -0600

    border fix

--- use_airtravel.py
+++ use_airtravel.py
@@ -38,7 +38,7 @@ def console_card_printer(passenger, seat, flight_number, aircraft):
              "Aircraft: {3} " \
              " |".format(passenger, flight_number, seat, aircraft)
     banner = "+" + "-" * (len(output) -2) + "+"
-    border = " |" + " " * (len(output) -2) + " |"
+    border = "|" + " " * (len(output) -2) + "|"
     lines = [banner, border, output, border, banner]
     card = '\n'.join(lines)
     print(card)
